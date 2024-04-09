ab13909f5602b284791fca695678ee218c6c8fc0
Juan <juanracca_@hotmail.com>
2024-04-09 12:40:10 +0200

    cambiando el puerto a 8080

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,6 +4,7 @@ const zod = require('zod');
 exports.getAllUsers = async (req,res) => {
     try {
         const users = await User.find();
+        console.log(users)
         res.status(200).json(users);
     } catch (error) {
         res.status(500).json({ message: error.message })
